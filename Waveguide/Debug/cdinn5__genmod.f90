67d7a466cc2c7506cb36edb3c640876fb30184fa
Khalturina <daryakhalturina@gmail.com>
2023-06-14 01:09:40 +0300

    NumSolution

---
+++ Waveguide/Debug/cdinn5__genmod.f90
@@ -0,0 +1,17 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Mon Mar 27 13:36:14 2023
+        ! This source file is for reference only and may not completely
+        ! represent the generated interface used by the compiler.
+        MODULE CDINN5__genmod
+          INTERFACE 
+            SUBROUTINE CDINN5(CF,A,B,EPS,PR,N,SB,RD)
+              INTEGER(KIND=4) :: N
+              EXTERNAL CF
+              COMPLEX(KIND=8) :: A
+              COMPLEX(KIND=8) :: B
+              REAL(KIND=8) :: EPS
+              REAL(KIND=8) :: PR
+              COMPLEX(KIND=8) :: SB(N)
+              COMPLEX(KIND=8) :: RD(N)
+            END SUBROUTINE CDINN5
+          END INTERFACE 
+        END MODULE CDINN5__genmod
